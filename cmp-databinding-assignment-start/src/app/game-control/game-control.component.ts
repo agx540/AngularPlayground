3a9382a23531a2963c939d70c15b60e1cd85bf32
Alexander Geltermair <alexander.geltermair@synx.com>
2019-12-21 18:54:25 +0100

    Aufgabe 4: Step 3

--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-
+  n: number;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onStartButton() {
+    console.log('onStart');
+    this.n = <any> setInterval(this.onEachSecond, 1000);
+  }
+
+  onEachSecond() {
+    console.log('eachSecond');
+
+
+  }
+
+  onStopButton() {
+    console.log('onStop');
+    clearTimeout(this.n);
+  }
 }
